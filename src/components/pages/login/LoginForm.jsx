4d97a3eef9db1d05d9c288d78bde7a56d388b8ba
DocteurK <fred_kreder@hotmail.com>
2023-11-14 20:36:43 +0100

    💄F03: LoginForm - Added Style to Input and Validate Btn

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { theme } from "../../../theme";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
 
 function LoginForm() {
   // D’abord on définit les states de base (état, données, variable…)
@@ -31,17 +32,24 @@ function LoginForm() {
         onSubmit={handleSubmit}
       >
         <h1>Bienvenue chez nous !</h1>
+        <hr />
         <h2>Connectez-vous</h2>
-        <input
-          value={firstName}
-          type="text"
-          name="inputField"
-          id="inputField"
-          onChange={handleChange}
-          placeholder="Entrez votre prénom"
-          required
-        />
-        <button>Accéder à mon espace</button>
+        <div className="input-with-icon">
+          <BsPersonCircle className="icon" />
+          <input
+            value={firstName}
+            type="text"
+            name="inputField"
+            id="inputField"
+            onChange={handleChange}
+            placeholder="Entrez votre prénom"
+            required
+          />
+        </div>
+        <button className="button-with-icon">
+          <span>Accéder à mon espace</span>
+          <IoChevronForward className="icon" />
+        </button>
       </LoginFormStyled>
     </>
   );
@@ -50,8 +58,104 @@ function LoginForm() {
 // Styling for the form
 const LoginFormStyled = styled.div`
   background: green;
-  display: flex;
-  flex-direction: column;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+  /* display: flex;
+  flex-direction: column; */
+
+  hr {
+    border: 1.5px solid #f56a2c;
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 70px;
+  }
+
+  h2 {
+    color: white;
+    margin: 20px 10px 10px;
+    font-size: 35px;
+  }
+
+  // Style pour la div Input with icon
+  .input-with-icon {
+    background-color: white;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+
+    .icon {
+      color: #93a2b1;
+      font-size: 15px;
+      margin-right: 8px;
+    }
+
+    input {
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+      /* width: 100%; */
+    }
+
+    &::placeholder {
+      background: yellow;
+      color: blue;
+    }
+  }
+
+  .button-with-icon {
+    width: 100%;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    white-space: nowrap;
+    text-decoration: none;
+    line-height: 1;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: 800;
+    color: white;
+    background-color: orange;
+    border: 1px solid orange;
+
+    &:hover:not(:disabled) {
+      background-color: white;
+      color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+      transition: all 200ms ease-out;
+    }
+
+    &:active {
+      color: white;
+      background-color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      margin-left: 10px;
+    }
+  }
 `;
 
 export default LoginForm;
