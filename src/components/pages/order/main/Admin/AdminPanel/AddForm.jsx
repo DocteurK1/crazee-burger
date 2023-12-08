c6b401f9bbdd1dba5eed2d41fc49844d4ab08c77
DocteurK <fred_kreder@hotmail.com>
2023-12-08 21:30:09 +0100

    💄 F08: Designed <addForm />

--- src/components/pages/order/main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/main/Admin/AdminPanel/AddForm.jsx
@@ -1,5 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FaHamburger } from "react-icons/fa";
+import AddFormInput from "../../../../../reusable-ui/AddFormInput.jsx";
+import { MdOutlineEuro } from "react-icons/md";
+import { BsFillCameraFill } from "react-icons/bs";
+import { theme } from "../../../../../../theme/index.js";
 
 export default function AddForm() {
-  return <div>AddForm</div>;
+  // State
+  let [value, setValue] = useState("");
+  let [productNameValue, setProductNameValue] = useState("");
+  let [productUrlValue, setProductUrlValue] = useState("");
+  let [productPrice, setProductPrice] = useState("");
+
+  // Comportements
+
+  const onProductNameChange = (event) => {
+    setProductNameValue(event.target.value);
+    console.log("Product Name : ", productNameValue);
+  };
+
+  const onProductUrlChange = (event) => {
+    setProductUrlValue(event.target.value);
+    console.log("Product Url : ", productUrlValue);
+  };
+
+  const onProductPriceeChange = (event) => {
+    setProductPrice(event.target.value);
+    console.log("Product Price : ", productPrice);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("Product Name : ", productNameValue);
+    console.log("Product Url : ", productUrlValue);
+    console.log("Product Price : ", productPrice);
+  };
+
+  // Affichage
+
+  return (
+    <AddFormStyled onSubmit={onSubmit}>
+      <div className="img-holder">Aucune image</div>
+      <div className="text-input">
+        <AddFormInput
+          value={value}
+          onChange={onProductNameChange}
+          placeholder={"Produit (ex: Super Burger)"}
+          Icon={<FaHamburger className="icon" />}
+        />
+        <AddFormInput
+          value={value}
+          onChange={onProductUrlChange}
+          placeholder={
+            "Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
+          }
+          Icon={<BsFillCameraFill className="icon" />}
+        />
+        <AddFormInput
+          value={value}
+          onChange={onProductPriceeChange}
+          placeholder={"Prix"}
+          Icon={<MdOutlineEuro className="icon" />}
+        />
+
+        {/* TO FIX: Refresh lorsque je submit ?! */}
+
+        <button className="submit-button" onClick={onSubmit}>
+          Ajouter un nouveau produit au menu
+        </button>
+      </div>
+    </AddFormStyled>
+  );
 }
+
+const AddFormStyled = styled.form`
+  border: 2px solid green;
+  /* background: pink; */
+  height: 100%;
+  display: flex;
+  padding-left: 87px;
+
+  .img-holder {
+    margin-top: 30px;
+    height: 120px;
+    width: 215px;
+    border: 1px solid green;
+
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    color: #93a2b1;
+    font-family: Open Sans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px; /* 150% */
+
+    border-radius: 5px;
+    /* border: 1px solid #e4e5e9; */
+    border: 1px solid black;
+  }
+
+  .text-input {
+    margin-top: 21px;
+    margin-left: 20px;
+    width: 645px;
+    /* background: black; */
+  }
+
+  .submit-button {
+    margin-top: 10px;
+    width: 275px;
+    height: 34px;
+    border-radius: 5px;
+    border: 1px solid #60bd4f;
+    background: #60bd4f;
+
+    /* display: inline-flex; */
+    padding: 10px 29px 9 29px;
+    align-items: flex-start;
+
+    color: #fff;
+    text-align: center;
+    font-family: Arial;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+
+    cursor: pointer;
+  }
+`;
---
+++ src/components/reusable-ui/AddFormInput.jsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import { theme } from "../../theme";
+
+export default function AddFormInput({ value, onChange, Icon, ...extraProps }) {
+  //   console.log("extraProps", extraProps.placeholder);
+
+  return (
+    <InputStyled>
+      {Icon && Icon}
+      <input onChange={onChange} type="text" {...extraProps} />
+    </InputStyled>
+  );
+}
+
+const InputStyled = styled.div`
+  // Style pour la div Input with icon
+
+  background-color: ${theme.colors.white};
+  border-radius: ${theme.borderRadius.round};
+  /* display: flex;
+  align-items: center;
+  padding: 18px 24px;
+  margin: 10px 0; */
+
+  display: flex;
+  margin-top: 10px;
+  padding: 8px 16px 8px 24px;
+  align-items: center;
+  gap: 13px;
+
+  border-radius: 5px;
+  background: #f5f5f7;
+
+  .icon {
+    color: ${theme.colors.greySemiDark};
+    font-size: ${theme.fonts.size.SM};
+    margin-right: 10px;
+  }
+
+  input {
+    /* border: none;
+    font-size: ${theme.fonts.size.SM};
+    font-weight: ${theme.fonts.weights.semiBold};
+    color: ${theme.colors.dark};
+    width: 100%; */
+    /* display: flex; */
+    width: 600px;
+    color: #a7a8ad;
+    background: #f5f5f7;
+    font-family: Arial;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    border: none;
+    &::placeholder {
+      /* background: ${theme.colors.white}; */
+      color: ${theme.colors.greyMedium};
+    }
+  }
+`;
