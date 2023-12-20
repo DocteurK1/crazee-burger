ccc9b73f0afa14b470ce2c5c9d8c8babe12965cb
DocteurK <fred_kreder@hotmail.com>
2023-12-20 15:09:40 +0100

    💄 F10: Designed <BasketHeader/>

--- src/components/pages/order/main/Admin/AdminPanel/Basket/BasketHeader.jsx
+++ src/components/pages/order/main/Admin/AdminPanel/Basket/BasketHeader.jsx
@@ -3,13 +3,28 @@ import styled from "styled-components";
 import { theme } from "../../../../../../../theme";
 
 export default function BasketHeader() {
-  return <BasketHeaderStyled>BasketHeader</BasketHeaderStyled>;
+  return (
+    <BasketHeaderStyled>
+      <div>Total</div>
+      <div>0,00 €</div>
+    </BasketHeaderStyled>
+  );
 }
 
 const BasketHeaderStyled = styled.div`
   /* border: 1px solid green; */
   height: 70px;
   background: ${theme.colors.background_dark};
-  color: ${theme.colors.primary};
+
   padding: 0px 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  color: ${theme.colors.primary};
+  color: #ffa01b;
+  font-family: ${theme.fonts.family.stylish};
+  font-size: ${theme.fonts.size.P4};
+  font-weight: ${theme.fonts.weights.regular};
+  letter-spacing: 2px;
 `;
